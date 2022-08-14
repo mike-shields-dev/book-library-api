aef707827f84b8ff2850cd012a4c061976826721
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-14 16:28:46 +0100

    refactor: Refactor router import/declaration

--- src/routes/readerRoute.js
+++ src/routes/readerRoute.js
@@ -1,5 +1,4 @@
-const { Router } = require("express");
-const router = Router();
+const router = require("express").Router();
 const addDbReaderToReqElseRes404 = require("../middlewares/addDbReaderToReqElseRes404");
 const {
   createOne,
