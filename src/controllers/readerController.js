595da75e710045570c9f996f089b2c7d0b8134eb
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-15 16:00:32 +0100

    feature: All readerController methods now use helper functions

--- src/controllers/readerController.js
+++ src/controllers/readerController.js
@@ -1,41 +1,9 @@
 const { Reader } = require("../models");
 
-exports.createOne = async (req, res) => {
-  try {
-    const newReader = await Reader.create(req.body);
-    res.status(201).send(newReader);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-};
+const { createItem, readAllItems, readOneItem, updateOneItem, deleteOneItem } = require('../utils/helpers');
 
-exports.readAll = async (req, res) => {
-  const allReaders = await Reader.findAll();
-  res.status(200).json(allReaders);
-};
-
-exports.readOne = async (req, res) => {
-  const reader = await Reader.findByPk(req.params.id);
-  if(!reader) {
-    return res.status(404).json({ error: 'Reader not found'});
-  }
-  res.status(200).json(reader);
-};
-
-exports.updateOne = async (req, res) => {
-  const reader = await Reader.findByPk(req.params.id);
-  if(!reader) {
-    return res.status(404).json({ error: 'Reader not found'});
-  }
-  const updatedReader = await reader.update(req.body);
-  res.status(200).json(updatedReader);
-};
-
-exports.deleteOne = async (req, res) => {
-  const reader = await Reader.findByPk(req.params.id);
-  if(!reader) {
-    return res.status(404).json({ error: 'Reader not found'});
-  }
-  await reader.destroy();
-  res.sendStatus(204);
-};
+exports.createOne = (req, res) => createItem(req, res, Reader);
+exports.readAll = (req, res) => readAllItems(req, res, Reader, "Reader");
+exports.readOne = (req, res) => readOneItem(req, res, Reader, "Reader");
+exports.updateOne = (req, res) => updateOneItem(req, res, Reader, "Reader");
+exports.deleteOne = (req, res) => deleteOneItem(req, res, Reader, "Reader");
