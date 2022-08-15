8596b2af5a0c443ef3970f332eccd56b507fd291
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-15 15:56:33 +0100

    feature: All bookController methods now use helper functions

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,36 +1,8 @@
 const { Book } = require('../models');
-const { createItem } = require('../utils/helpers');
+const { createItem, readAllItems, readOneItem, updateOneItem, deleteOneItem } = require('../utils/helpers');
 
 exports.createOne = (req, res) => createItem(req, res, Book);
-
-exports.readAll = async (req, res) => {
-    const allBooks = await Book.findAll();
-    res.status(200).json(allBooks);
-}
-
-exports.readOne = async (req, res) => {
-    const book = await Book.findByPk(req.params.id);
-    if(!book) {
-        return res.status(404).json({ error: 'Book not found'});
-    }
-    res.status(200).json(book);
-}
-
-exports.updateOne = async (req, res) => {
-    const book = await Book.findByPk(req.params.id);
-    if(!book) {
-        return res.status(404).json({ error: 'Book not found'});
-    }
-    const updatedBook = await book.update(req.body);
-    res.status(200).json(updatedBook);
-}
-
-
-exports.deleteOne = async (req, res) => {
-    const book = await Book.findByPk(req.params.id);
-    if(!book) {
-        return res.status(404).json({ error: 'Book not found'});
-    }
-    await book.destroy();
-    res.sendStatus(204);
-}
+exports.readAll = (req, res) => readAllItems(req, res, Book, "Book");
+exports.readOne = (req, res) => readOneItem(req, res, Book, "Book");
+exports.updateOne = (req, res) => updateOneItem(req, res, Book, "Book");
+exports.deleteOne = (req, res) => deleteOneItem(req, res, Book, "Book");q
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,3 +6,34 @@ exports.createItem = async (req, res, model) => {
         res.status(400).json({ error: err.message });
     }
 }    
+
+exports.readAllItems = async (req, res, model) => {
+    const allItems = await model.findAll();
+    res.status(200).json(allItems);
+}
+
+exports.readOneItem = async (req, res, model, itemName) => {
+    const item = await model.findByPk(req.params.id);
+    if(!item) {
+        return res.status(404).json({ error: `${itemName} not found`});
+    }
+    res.status(200).json(item);
+}
+
+exports.updateOneItem = async (req, res, model, itemName) => {
+    const item = await model.findByPk(req.params.id);
+    if(!item) {
+        return res.status(404).json({ error: `${itemName} not found`});
+    }
+    const updatedItem = await item.update(req.body);
+    res.status(200).json(updatedItem);
+}
+
+exports.deleteOneItem = async (req, res, model, itemName) => {
+    const item = await model.findByPk(req.params.id);
+    if(!item) {
+        return res.status(404).json({ error: `${itemName} not found`});
+    }
+    await item.destroy();
+    res.sendStatus(204);
+}
