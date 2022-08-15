cd5484c307cfb11e7d2607ea21e3f272cc40fce1
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-15 15:57:50 +0100

    fix: Ammend typo

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -5,4 +5,4 @@ exports.createOne = (req, res) => createItem(req, res, Book);
 exports.readAll = (req, res) => readAllItems(req, res, Book, "Book");
 exports.readOne = (req, res) => readOneItem(req, res, Book, "Book");
 exports.updateOne = (req, res) => updateOneItem(req, res, Book, "Book");
-exports.deleteOne = (req, res) => deleteOneItem(req, res, Book, "Book");q
+exports.deleteOne = (req, res) => deleteOneItem(req, res, Book, "Book");
