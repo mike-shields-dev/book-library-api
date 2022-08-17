21da330d0a6b8fa1e3e8c4990780459e1476c982
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-17 10:09:34 +0100

    refactor: modelHelpers adding sanitize function

--- src/utils/handleIdRequest.js
+++
@@ -1,7 +0,0 @@
-module.exports  = async function (req, res, model) {
-    const foundItem = await model.findByPk(req.params.id);
-    if(!foundItem) {
-        return res.status(404).json({ error: `${model.name} not found`});
-    }
-    req.item = foundItem;
-}
--- src/utils/modelHelpers.js
+++ src/utils/modelHelpers.js
@@ -1,32 +1,44 @@
-const handleIdRequest = require('./handleIdRequest');
+async function handleIdRequest(req, res, model) {
+  const foundItem = await model.findByPk(req.params.id);
+  if (!foundItem) {
+    return res.status(404).json({ error: `${model.name} not found` });
+  }
+  req.item = foundItem;
+}
+
+function sanitize(obj) {
+  delete obj?.password;
+  return obj;
+}
+
 
 exports.createItem = async (req, res, model) => {
-    try {
-        const newItem = await model.create(req.body);
-        res.status(201).json(newItem);
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
-}    
+  try {
+    const newItem = await model.create(req.body);
+    res.status(201).json(newItem);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
 
 exports.readAllItems = async (_, res, model) => {
-    const allItems = await model.findAll();
-    res.status(200).json(allItems);
-}
+  const allItems = [...(await model.findAll())].map(sanitize);
+  res.status(200).json(allItems);
+};
 
 exports.readOneItem = async (req, res, model) => {
-    await handleIdRequest(req, res, model)
-    res.status(200).json(req.item);
-}
+  await handleIdRequest(req, res, model);
+  res.status(200).json(sanitize(req.item));
+};
 
 exports.updateOneItem = async (req, res, model) => {
-    await handleIdRequest(req, res, model)
-    await req.item.update(req.body);
-    res.status(200).json(req.item);
-}
+  await handleIdRequest(req, res, model);
+  await req.item.update(req.body);
+  res.status(200).json(sanitize(req.item));
+};
 
 exports.deleteOneItem = async (req, res, model) => {
-    await handleIdRequest(req, res, model)
-    await req.item.destroy();
-    res.sendStatus(204);
-}
+  await handleIdRequest(req, res, model);
+  await req.item.destroy();
+  res.sendStatus(204);
+};
