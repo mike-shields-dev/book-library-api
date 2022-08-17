9a505ba80b808138be9b519b406396b895832f22
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-17 12:44:40 +0100

    refactor: call get method on the item instance inside the sanitizeIfNeeded function

--- src/utils/modelHelpers.js
+++ src/utils/modelHelpers.js
@@ -7,22 +7,24 @@ async function handleIdRequest(req, res, model) {
 }
 
 function sanitizeIfNeeded(obj, model) {
+  const dataValues = obj.get()
   if (model.name === "Reader") {
-    delete obj?.password;
+    delete dataValues?.password;
   }
   return obj;
 }
 
 exports.createItem = async (req, res, model) => {
   try {
-    const newItem = await model.create(req.body);
+    let newItem = await model.create(req.body);
+    newItem = sanitizeIfNeeded(newItem, model);
     res.status(201).json(newItem);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 };
 
-exports.readAllItems = async (_, res, model) => {
+exports.readAllItems = async (req, res, model) => {
   let allItems = [...(await model.findAll())].map((item) =>
     sanitizeIfNeeded(item, model)
   );
@@ -31,13 +33,15 @@ exports.readAllItems = async (_, res, model) => {
 
 exports.readOneItem = async (req, res, model) => {
   await handleIdRequest(req, res, model);
-  res.status(200).json(sanitizeIfNeeded(req.item, model));
+  const item = sanitizeIfNeeded(req.item, model)
+  res.status(200).json(item);
 };
 
 exports.updateOneItem = async (req, res, model) => {
   await handleIdRequest(req, res, model);
   await req.item.update(req.body);
-  res.status(200).json(sanitizeIfNeeded(req.item, model));
+  const updatedItem = sanitizeIfNeeded(req.item, model)
+  res.status(200).json(updatedItem);
 };
 
 exports.deleteOneItem = async (req, res, model) => {
