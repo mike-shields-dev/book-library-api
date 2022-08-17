02693039f38ecff05751fb285090a4405fb36277
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-17 10:10:39 +0100

    refactor: Remove redundant argument from modelHelper functions

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -2,7 +2,7 @@ const { Book } = require('../models');
 const { createItem, readAllItems, readOneItem, updateOneItem, deleteOneItem } = require('../utils/modelHelpers');
 
 exports.createOne = (req, res) => createItem(req, res, Book);
-exports.readAll = (req, res) => readAllItems(req, res, Book, "Book");
-exports.readOne = (req, res) => readOneItem(req, res, Book, "Book");
-exports.updateOne = (req, res) => updateOneItem(req, res, Book, "Book");
-exports.deleteOne = (req, res) => deleteOneItem(req, res, Book, "Book");
+exports.readAll = (req, res) => readAllItems(req, res, Book);
+exports.readOne = (req, res) => readOneItem(req, res, Book);
+exports.updateOne = (req, res) => updateOneItem(req, res, Book);
+exports.deleteOne = (req, res) => deleteOneItem(req, res, Book);
--- src/controllers/readerController.js
+++ src/controllers/readerController.js
@@ -3,7 +3,7 @@ const { Reader } = require("../models");
 const { createItem, readAllItems, readOneItem, updateOneItem, deleteOneItem } = require('../utils/modelHelpers');
 
 exports.createOne = (req, res) => createItem(req, res, Reader);
-exports.readAll = (req, res) => readAllItems(req, res, Reader, "Reader");
-exports.readOne = (req, res) => readOneItem(req, res, Reader, "Reader");
-exports.updateOne = (req, res) => updateOneItem(req, res, Reader, "Reader");
-exports.deleteOne = (req, res) => deleteOneItem(req, res, Reader, "Reader");
+exports.readAll = (req, res) => readAllItems(req, res, Reader);
+exports.readOne = (req, res) => readOneItem(req, res, Reader);
+exports.updateOne = (req, res) => updateOneItem(req, res, Reader);
+exports.deleteOne = (req, res) => deleteOneItem(req, res, Reader);
